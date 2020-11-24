73f6a257dcaaabe1709599f930e76dbe9f5f889f
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2020-11-24 06:24:16 -0600

    If aliens reach bottom of screen, treat it like _ship_hit()

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,7 +116,10 @@ class AlienInvasion:
 
 		# Look for alien-ship collisions.
 		if pygame.sprite.spritecollideany(self.ship, self.aliens):	# spritecollideany() functions takes 2 args, sprite and a group.
-			self._ship_hit()								# if spritecollideany() returns None, if block doesn't execute
+			self._ship_hit()							# if spritecollideany() returns None, if block doesn't execute
+
+		# Look for aliens hitting the bottom of the screen.
+		self._check_aliens_bottom()
 
 
 	def _check_fleet_edges(self):
@@ -194,6 +197,15 @@ class AlienInvasion:
 
 		# Pause.
 		sleep(0.5)
+
+	def _check_aliens_bottom(self):
+		"""Check if any aliens have reached the bottom of the screen."""
+		screen_rect = self.screen.get_rect()
+		for alien in self.aliens.sprites():
+			if alien.rect.bottom >= screen_rect.bottom:
+				# Treat this the same as if the ship got hit.
+				self._ship_hit()
+				break
 
 
 if __name__ == '__main__':
