6c9b057ff827ac4c5c701dc23a6d564da80d14fb
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2020-11-28 13:40:14 -0600

    Created Scoreboard.py and made changes in AI to display scoreboard

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -25,6 +26,9 @@ class AlienInvasion:
 
 		# Create an instance to store game statistics.
 		self.stats = GameStats(self)
+
+		# and create a scoreboard
+		self.sb = Scoreboard(self)
 
 		self.ship = Ship(self)										# the self arg refers to the current instance of AlienInvasion
 																	# Parameter gives Ship access to the game's resources, such as screen object
@@ -221,6 +225,9 @@ class AlienInvasion:
 			bullet.draw_bullet()								# to draw all fired bullets, we loop though the sprites in bullets and call draw_bullet on each
 		# draws each element in group at the position defined by its rect attribute
 		self.aliens.draw(self.screen)							# the draw() method requires one arg: a surface on which to draw
+
+		# Draw the score information.
+		self.sb.show_score()
 
 		# Draw the play button if the game is inactive.
 		if not self.stats.game_active:
--- scoreboard.py
+++ scoreboard.py
@@ -5,4 +5,30 @@ class Scoreboard:
 
 	def __init__(self, ai_game):
 		"""Initialize scorekeeping attributes."""
-		
+		self.screen = ai_game.screen
+		self.screen_rect = self.screen.get_rect()
+		self.settings = ai_game.settings
+		self.stats = ai_game.stats
+
+		# Font settings for scoring information.
+		self.text_color = (255, 0, 0)
+		self.bg_color = (110, 0, 110)
+		self.font = pygame.font.SysFont(None, 48)
+
+		# Prepare the initial score image.
+		self.prep_score()
+
+	def prep_score(self):
+		"""Turn the score into a rendered image."""
+		score_str = str(self.stats.score)						# We convert stats.score into string...
+		self.score_image = self.font.render(score_str, True,	# ...then we pass this string to render() which creates the image (including bg color and text color)
+				self.text_color, self.bg_color)					# (the TRUE value is a boolean for antialiasing)
+
+		#Display the score at the top right of the screen.
+		self.score_rect = self.score_image.get_rect()			# Creating rect location to position score to upperight corner of the screen 
+		self.score_rect.right = self.screen_rect.right - 20		# We set its right edge 20 pixels from the right edge of the screen
+		self.score_rect.top = 20								# We then place the top edge 20 pixels down from the top of screen
+
+	def show_score(self):
+		"""Draw score to the screen."""
+		self.screen.blit(self.score_image, self.score_rect)		# Method draws the score image onscreen at the location score_rect specifies.
